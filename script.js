9e544ca23865146c3ba1e39e95ebf972e98285c6
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-16 18:47:26 -0300

    Add função startGame()

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ const gameEndMessage = document.querySelector('[data-game-end-message]');
 const gameEndElement = document.querySelector('#gameEndElement');
 
 const restartButton = document.querySelector('#restarButton');
+
+function startGame() {
+   xTurn = true;
+   board.classList.add('x');
+
+   cells.forEach(cell => {
+      cell.classList.remove(xClass);
+      cell.classList.remove(oClass);
+      board.classList.replace('o', 'x');
+
+      cell.addEventListener('click', handleClick, { once: true });
+   });
+
+   restartButton.addEventListener('click', startGame);
+   document.querySelector('main').classList.remove('end');
+}
+
+startGame();
