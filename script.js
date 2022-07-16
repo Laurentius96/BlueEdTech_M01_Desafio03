80243d46ef2e3849207e847f456676d28b575e88
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-16 19:45:55 -0300

    Add condições de vitória e empate

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ const gameEndMessage = document.querySelector('[data-game-end-message]');
 
 const gameEndElement = document.querySelector('#gameEndElement');
 
-const restartButton = document.querySelector('#restarButton');
+const restartButton = document.querySelector('#restartButton');
 
-startGame();
+const lines = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+];
 
 function startGame() {
    xTurn = true;
@@ -35,6 +44,13 @@ function handleClick(e) {
    let turnClass = xTurn ? xClass : oClass;
 
    placeMark(cell, turnClass);
+
+   if (checkWin(turnClass)) {
+      endGame(false);
+   } else if (checkDraw()) {
+      endGame(true);
+   }
+
    swapTurn();
    setBoardHover();
 }
@@ -52,3 +68,30 @@ function setBoardHover() {
       ? board.classList.replace('o', 'x')
       : board.classList.replace('x', 'o');
 }
+
+function checkWin(turnClass) {
+   return lines.some(line => {
+      return line.every(index => {
+         return cells[index].classList.contains(turnClass);
+      });
+   });
+}
+
+function checkDraw() {
+   return cells.every(cell => {
+      return cell.classList.contains(xClass) || cell.classList.contains(oClass);
+   });
+}
+
+function endGame(draw) {
+   if (draw) {
+      gameEndMessage.innerText = 'Empate!!';
+   } else {
+      gameEndMessage.innerText = `${xTurn ? 'X' : 'O'} venceu!`;
+   }
+
+   gameEndElement.classList.add('show');
+   document.querySelector('main').classList.add('end');
+}
+
+startGame();
